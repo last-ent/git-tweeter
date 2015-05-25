f44fb422abcd53d7054d95807fe7a780d43a311d
Ent <Ent@Ents-MacBook-Pro.local>
2015-05-26 01:10:10 +0530

    Akka & Actors.

--- build.sbt
+++ build.sbt
@@ -7,6 +7,7 @@ scalaVersion := "2.11.6"
 libraryDependencies ++= Seq(
   "junit" % "junit" % "4.12" % "test",
   "org.mockito" % "mockito-core" % "1.10.19",
+  "com.typesafe.akka" %% "akka-actor" % "2.3.11",
   "org.twitter4j" % "twitter4j-stream" % "4.0.3",
   "com.novocode" % "junit-interface" % "0.11" % "test",
   "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test",
--- src/main/scala/RepoWatcher.scala
+++ src/main/scala/RepoWatcher.scala
@@ -1,20 +1,28 @@
 package gittweet
 
-import java.nio.file.{Path, WatchEvent, FileSystems}
-import java.nio.file.StandardWatchEventKinds._
+import java.io.File
+import scala.io.Source
 import util.control.Breaks._
 import collection.JavaConversions._
-
 import scala.util.control.NonFatal
+import akka.actor.{ActorRef, Actor}
+import java.nio.file.{Path, FileSystems}
+import java.nio.file.StandardWatchEventKinds._
+import gittweet.SafeIO.{readSafely, writeSafely}
+
+case object MonitorRepos
+case class RegisterRepo(path: String)
+case class Tweet(commitMessage: Vector[String])
+case class RegisterRepos(repos: Vector[String])
 
 /**
- * RepoSnooper should be able to
- *  - Register new & existing set of Repos -> Repo Watcher
- *  - Watch for changes in the Repos -> Repo Watcher
+  * RepoWatcher =>
+ *  - Register new & existing set of Repos -> Repo Snooper
+ *  - Watch for changes in the Repos -> Repo Snooper
  *  - Fire a message to tweet when a new commit is made -> Commit Tweeter
- *  - Repo Watcher & Commit Tweeter are two Actors
+ *  - Repo Snooper & Commit Tweeter are two Actors
  *
- * Repo Watcher =>
+ * Repo Snooper =>
  *  - Check if each path is a valid git repo and register for directory notification only if it is a git repo
  *  - Check if .git/objects/ has changed, if it has then check for latest commit
  *  ? Check if latest commit hash is different from previous tweeted commit for the given branch
@@ -28,40 +36,78 @@ import scala.util.control.NonFatal
  *  ? Store tweet timestamp & commit hash
  *  - Tweet to Twitter as per defined template
  */
-class RepoWatcher {
-  def repo
-  /**
-   * watchDir is a base template based on which we need to develop our Actor Model for Repo Watcher
-   */
-  def watchDir(path: String = System.getProperty("user.dir")) = {
-    val watcher = FileSystems.getDefault.newWatchService
-    val dir = FileSystems.getDefault.getPath(path)
 
-    try {
-      val key = dir.register(watcher, ENTRY_CREATE, ENTRY_MODIFY)
-    } catch {
-      case NonFatal(ex) => {
-        println(s"Failed to register: $ex")
-      }
-    }
-    breakable {
-      while (true) {
-        val watchKey = watcher.take()
-
-        watchKey.pollEvents() foreach { event =>
-          val relativePath = event.context.asInstanceOf[Path]
-          val path = watchKey.watchable.asInstanceOf[Path].resolve(relativePath)
-          event.kind match {
-            case ENTRY_CREATE => println(s"CREATED: $path")
-            case ENTRY_MODIFY => {
-              println(s"MODIFIED: $path")
-              println("File: "+ event.context)
-            }
-            case x => println(s"OTHERWISE: $x")
-          }
+class RepoCataloguer(catalogPath: String, repoSnooper: ActorRef) extends Actor {
+  def receive = {
+    case RegisterRepo(path) => {
+      val repoCatalogue = new File(catalogPath)
+      try {
+        if(new File(catalogPath).exists && repoCatalogue.exists) {
+          writeSafely(repoCatalogue, catalogPath+"\n")
+          repoSnooper ! RegisterRepo(path)
         }
-        watchKey.reset
+      } catch {
+        case NonFatal(ex) => println(s"Non-Fatal Exception: $ex")
       }
     }
   }
 }
+
+class RepoSnooper(twitterClient: ActorRef, cataloguePath:String) extends Actor {
+  val watcher = FileSystems.getDefault.newWatchService
+
+  def addToWatcher(path: String) = {
+    try{
+      FileSystems.getDefault.getPath(path)
+        .register(watcher, ENTRY_CREATE, ENTRY_MODIFY)
+    } catch {
+      case NonFatal(ex) => println(s"Non-Fatal Exception: $ex")
+    }
+  }
+
+  def detectChanges = {
+    val watchKey = watcher.take()
+    watchKey.pollEvents() foreach {event =>
+      breakable{
+        if (event.context.toString != ".git" || event.kind == OVERFLOW)
+          break
+
+        val relativePath = event.context.asInstanceOf[Path]
+        val path = watchKey.watchable.asInstanceOf[Path].resolve(relativePath)
+        val commitDetails: Vector[String] = (new Gitter).getCommitMessage(path.toString)
+        self ! Tweet(commitDetails)
+      }
+    }
+    watchKey.reset
+  }
+
+  override def preStart = {
+    val catalogue = new File(cataloguePath)
+    if (catalogue.exists){
+      val repos = readSafely(cataloguePath) { Source.fromFile } {_.getLines.toVector}
+      self ! RegisterRepos(repos)
+    }
+  }
+
+  def receive = {
+    case RegisterRepo(path) => {
+      addToWatcher(path)
+      self ! MonitorRepos
+    }
+    case MonitorRepos => {
+      detectChanges
+      self ! MonitorRepos
+    }
+    case RegisterRepos(repos) => {
+      if(repos.isEmpty)
+        self ! MonitorRepos
+      else{
+        addToWatcher(repos.head)
+        self ! RegisterRepos(repos.tail)
+      }
+    }
+    case Tweet(commitDetails) =>
+      self ! Tweet(commitDetails)
+  }
+}
+
